83962b7114c232536a5729e5f1cc5f82792edffe
Oscar Koeroo <okoeroo@gmail.com>
2013-07-02 15:16:36 +0200

    Complete revamp of the GenAuthZ htaccess module. Now works according to the profile (see docs/)

--- genauthz_htaccess_main.c
+++ genauthz_htaccess_main.c
@@ -1,5 +1,6 @@
 #include "genauthz/genauthz_plugin.h"
 #include <htaccess/htaccess.h>
+#include <genauthz_htaccess.h>
 
 int
 htaccess_plugin_init(tq_xacml_callout_t *);
@@ -9,7 +10,7 @@ int
 htaccess_plugin_rule_hit(request_mngr_t *, tq_xacml_rule_t *, tq_xacml_callout_t *);
 
 #ifndef GENAUTZ_HTACCESS_TITLE
-    #define GENAUTZ_HTACCESS_TITLE "GenAuthZ htaccess"
+    #define GENAUTZ_HTACCESS_TITLE "ga_htaccess:"VERSION
 #endif
 
 int
@@ -22,8 +23,8 @@ htaccess_plugin_init(tq_xacml_callout_t *callout) {
     argv = genauthz_callout_get_argv(callout);
 
     if (argc < 2) {
-        printf("%s: Initialization error, expecting 1 argument for the htaccess file\n",
-                GENAUTZ_HTACCESS_TITLE);
+        syslog(LOG_ERR, "[%s][pid:%lu][initerror=expecting 1 argument for the htaccess file]",
+                        GENAUTZ_HTACCESS_TITLE, (uint64_t)getpid());
         return 1;
     }
 
@@ -32,12 +33,15 @@ htaccess_plugin_init(tq_xacml_callout_t *callout) {
         return 1;
 
     if (htaccess_parse_file(ht_ctx, argv[1]) != 0) {
-        printf("%s: htaccess_parse_file() failed! Error: %s\n",
-                GENAUTZ_HTACCESS_TITLE,
-                htaccess_get_error(ht_ctx));
+        syslog(LOG_ERR, "[%s][pid:%lu][initerror=htaccess_parse_file() failed][msg=%s]",
+                        GENAUTZ_HTACCESS_TITLE, (uint64_t)getpid(),
+                        htaccess_get_error(ht_ctx));
+        return 1;
     }
 
+#ifdef DEBUG
     htaccess_print_ctx(ht_ctx);
+#endif
 
     genauthz_callout_set_aux(callout, ht_ctx);
     return 0;
@@ -69,15 +73,11 @@ htaccess_plugin_search(struct tq_xacml_request_s *xacml_req,
             TAILQ_FOREACH(req_attr, &(req_cat->attributes), next) {
                 /* Match the Rule->Subject->subjectid with the same
                  * Request->Subject->subjectid */
+
                 if (strcasecmp(attribute_id, (char *)req_attr->id) == 0) {
                     if (TAILQ_EMPTY(&(req_attr->values))) {
                         return NULL;
                     }
-                } else {
-                    if (TAILQ_EMPTY(&(req_attr->values))) {
-                        return NULL;
-                    }
-
                     TAILQ_FOREACH(req_value, &(req_attr->values), next) {
                         /* check if the datatype matches a string */
                         if (GA_XACML_DATATYPE_STRING == req_value->datatype) {
@@ -93,26 +93,6 @@ htaccess_plugin_search(struct tq_xacml_request_s *xacml_req,
 }
 
 
-static char *
-htaccess_plugin_get_subject_username(struct tq_xacml_request_s *xacml_req) {
-    return htaccess_plugin_search(xacml_req, GA_XACML_CATEGORY_SUBJECT, "x-urn:nl:mpi:tla:xacml:subject:username");
-}
-
-static char *
-htaccess_plugin_get_action_httpmethod(struct tq_xacml_request_s *xacml_req) {
-    return htaccess_plugin_search(xacml_req, GA_XACML_CATEGORY_ACTION, "x-urn:nl:mpi:tla:xacml:action:httpmethod");
-}
-
-static char *
-htaccess_plugin_get_resource_directory(struct tq_xacml_request_s *xacml_req) {
-    return htaccess_plugin_search(xacml_req, GA_XACML_CATEGORY_RESOURCE, "x-urn:nl:mpi:tla:xacml:resource:directory");
-}
-
-static char *
-htaccess_plugin_get_resource_file(struct tq_xacml_request_s *xacml_req) {
-    return htaccess_plugin_search(xacml_req, GA_XACML_CATEGORY_RESOURCE, "x-urn:nl:mpi:tla:xacml:resource:file");
-}
-
 static void
 htaccess_plugin_set_decision(struct tq_xacml_response_s *xacml_res, enum ga_xacml_decision_e decision) {
     xacml_res->decision = decision;
@@ -124,40 +104,68 @@ htaccess_plugin_rule_hit(request_mngr_t *request_mngr,
                         tq_xacml_callout_t *callout) {
     htaccess_ctx_t *ht_ctx = (htaccess_ctx_t *)genauthz_callout_get_aux(callout);
     htaccess_decision_t rc;
-    char *username, *httpmethod, *directory, *file;
+    char *username, *access, *directory, *file;
 
-    printf("Rule \"%s\" hit! -- %s\n", rule->name, __func__);
+    username    = htaccess_plugin_search(request_mngr->xacml_req,
+                                         GA_XACML_CATEGORY_SUBJECT,
+                                         "x-urn:nl:mpi:tla:xacml:subject:username");
+    access      = htaccess_plugin_search(request_mngr->xacml_req,
+                                         GA_XACML_CATEGORY_ACTION,
+                                         "x-urn:nl:mpi:tla:xacml:action:access");
+    directory   = htaccess_plugin_search(request_mngr->xacml_req,
+                                         GA_XACML_CATEGORY_RESOURCE,
+                                         "x-urn:nl:mpi:tla:xacml:resource:directory");
+    file        = htaccess_plugin_search(request_mngr->xacml_req,
+                                         GA_XACML_CATEGORY_RESOURCE,
+                                         "x-urn:nl:mpi:tla:xacml:resource:file");
 
-    username    = htaccess_plugin_get_subject_username(request_mngr->xacml_req);
-    httpmethod  = htaccess_plugin_get_action_httpmethod(request_mngr->xacml_req);
-    directory   = htaccess_plugin_get_resource_directory(request_mngr->xacml_req);
-    file        = htaccess_plugin_get_resource_file(request_mngr->xacml_req);
+    /* Todo: nice error message on malformed input */
 
-    if (httpmethod == NULL || strcmp(httpmethod, "GET") == 0) {
-        rc = htaccess_approve_access(ht_ctx, directory, file, username);
-        printf("Using: dir \"%s\" file \"%s\" user \"%s\" ", directory, file, username);
+    /* print_normalized_xacml_request(request_mngr->xacml_req); */
+    /* print_loaded_policy(request_mngr->app->parent->xacml_policy); */
+    /* printf("Using: dir \"%s\" file \"%s\" user \"%s\", access \"%s\" ", directory, file, username, access); */
 
-        switch (rc) {
-            case HTA_INAPPLICABLE:
-                printf("decision: Inapplicable");
-                htaccess_plugin_set_decision(request_mngr->xacml_res, GA_XACML_DECISION_NOTAPPLICABLE);
-                break;
-            case HTA_PERMIT:
-                printf("decision: Permit");
-                htaccess_plugin_set_decision(request_mngr->xacml_res, GA_XACML_DECISION_PERMIT);
-                break;
-            case HTA_DENY:
-                printf("decision: Deny");
-                htaccess_plugin_set_decision(request_mngr->xacml_res, GA_XACML_DECISION_DENY);
-                break;
-            default:
-                printf("decision: Unknown!");
-                htaccess_plugin_set_decision(request_mngr->xacml_res, GA_XACML_DECISION_INDETERMINATE);
-        }
-        printf("\n");
-        return 0;
+    /* TODO Check 'access' */
+    rc = htaccess_approve_access(ht_ctx, directory, file, username);
+    switch (rc) {
+        case HTA_INAPPLICABLE:
+            /* 128(basic line: name, thr id, etc) + 64 + 768 + 768 + 256 =< 2048(max syslog line) */
+            syslog(LOG_NOTICE, "[%s][pid:%lu][threadid:%lu][rule=%.64s][directory=%.768s][file=%.768s][user=%.256s][decision=Inapplicable]",
+                                GENAUTZ_HTACCESS_TITLE,
+                                (uint64_t)getpid(),
+                                pthread_self(),
+                                rule->name,
+                                directory, file, username);
+            htaccess_plugin_set_decision(request_mngr->xacml_res, GA_XACML_DECISION_NOTAPPLICABLE);
+            break;
+        case HTA_PERMIT:
+            syslog(LOG_NOTICE, "[%s][pid:%lu][threadid:%lu][rule=%.64s][directory=%.768s][file=%.768s][user=%.256s][decision=Permit]",
+                                GENAUTZ_HTACCESS_TITLE,
+                                (uint64_t)getpid(),
+                                pthread_self(),
+                                rule->name,
+                                directory, file, username);
+            htaccess_plugin_set_decision(request_mngr->xacml_res, GA_XACML_DECISION_PERMIT);
+            break;
+        case HTA_DENY:
+            syslog(LOG_NOTICE, "[%s][pid:%lu][threadid:%lu][rule=%.64s][directory=%.768s][file=%.768s][user=%.256s][decision=Deny]",
+                                GENAUTZ_HTACCESS_TITLE,
+                                (uint64_t)getpid(),
+                                pthread_self(),
+                                rule->name,
+                                directory, file, username);
+            htaccess_plugin_set_decision(request_mngr->xacml_res, GA_XACML_DECISION_DENY);
+            break;
+        default:
+            syslog(LOG_NOTICE, "[%s][pid:%lu][threadid:%lu][rule=%.64s][directory=%.768s][file=%.768s][user=%.256s][decision=Indeterminate]",
+                                GENAUTZ_HTACCESS_TITLE,
+                                (uint64_t)getpid(),
+                                pthread_self(),
+                                rule->name,
+                                directory, file, username);
+            htaccess_plugin_set_decision(request_mngr->xacml_res, GA_XACML_DECISION_INDETERMINATE);
     }
-    return 1;
+    /* print_normalized_xacml_response(request_mngr->xacml_res); */
+    return 0;
 }
-
 
